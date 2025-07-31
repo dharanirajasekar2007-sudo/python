debc09f63f4358cdae2e15731a112d08a8b1e817
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:24:43 +0530

    Create celcius.py
    fahrenheit

---
+++ celcius.py
@@ -0,0 +1,9 @@
+                                                                                                 CENTIGRADE TO FARENHEIT
+
+
+ALGORITHM
+
+*STEP 1: Start the execution
+*STEP 2: Read the value of celsius
+*STEP 3: Calculate Fahrenheit=(1.8*celcius+32)
+*STEP 4: print the value of fahrenheit
