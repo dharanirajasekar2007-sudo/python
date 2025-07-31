548bb7da94eaf72ce97e1046ae2be9f15e8b6758
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:36:04 +0530

    Create placement.py
    class

---
+++ placement.py
@@ -0,0 +1,5 @@
+placement_class=["digital and fuctional skills and lab","python and lab","loop and nerwork",]
+for i in placement_class:
+    print(i)
+for i, placement in enumerate(placement_class):
+    print(i+1,placement)
