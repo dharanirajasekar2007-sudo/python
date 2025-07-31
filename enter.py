afa23e205b45de6c44c508b01d9afb4e1e4d49c2
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:33:14 +0530

    Create enter.py
    greater

---
+++ enter.py
@@ -0,0 +1,10 @@
+A=int(input("Enter number1:"))
+B=int(input("Enter Number2:"))
+C=int(input("Enter Number3:"))
+if  A>B:
+    print("A is greater")
+elif B>C:
+    print("B is greater")
+else:
+    print("C is greater")
+    
