7e68f47f562a138f4fa817931bd9ff787b98c5d4
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:27:59 +0530

    Create input.py
    amount

---
+++ input.py
@@ -0,0 +1,8 @@
+pin=input("what you want:")
+if pin=="withdraw":
+    print("take a amount:")
+    amount=int(input("how much money you want"))
+    print("Take the cash "),amount
+else:
+    print("deposite your money")
+    
