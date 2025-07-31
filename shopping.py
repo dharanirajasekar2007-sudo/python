c4ae992e0941e235eb42564957dab65fa255d0d6
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:36:34 +0530

    Create shopping.py
    list

---
+++ shopping.py
@@ -0,0 +1,5 @@
+shopping_list=["soaps","snacks","turmeric powder","tooth paste","dress","bangles"]
+for i in shopping_list:
+    print(i)
+for i,list in enumerate(shopping_list):
+    print(i+1,list)
