5a94e808f3db18b3adad134c14932eea2469fbcb
dharanirajasekar2007-sudo <dharanirajasekar2007@gmail.com>
2025-07-31 15:30:42 +0530

    Create greatest.py
    smallest

---
+++ greatest.py
@@ -0,0 +1,13 @@
+                                                                                GREATEST OF THREE NUMBERS
+
+
+ALGORITHM
+
+*STEP 1: Start the execution
+*STEP 2: read the value a,b,c
+*STEP 3: If (a>b) and (a>c) then
+*STEP 4: Print a is greater value
+*STEP 5: Else if (b>a) and (b>c) then
+*STEP 6: Print b is greatest value 
+*STEP 7: Else if (c>a) and (c>b) then
+* STEP 8: print c is greatest value
